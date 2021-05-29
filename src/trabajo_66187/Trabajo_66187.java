4174e3518bee3673fde1dbec1bdd1635169c3e6f
DELL <DELL@DELL-PC>
2021-05-28 21:27:37 -0500

    new

--- src/trabajo_66187/Trabajo_66187.java
+++ src/trabajo_66187/Trabajo_66187.java
@@ -7,7 +7,7 @@ package trabajo_66187;
 
 /**
  *
- * @author Sammy Guergachi <sguergachi at gmail.com>
+ * @author Alondra Palma
  */
 public class Trabajo_66187 {
 
@@ -15,7 +15,25 @@ public class Trabajo_66187 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("----------------------TRABAJOTI--------------------");
+        trabajoTI developper = new trabajoTI();
         
-    }
+        developper.setNombre("Emiliano");
+        developper.setDescripcion("Desarrollo de aplicaciones");
+        developper.setHabilidades("base de datos");
+        developper.setExperiencia(2);
+        developper.setNivel("Jr");
+
+        System.out.println(developper.toString());
+        
+        trabajoTI designer = new trabajoTI ("Alejandro","Desarrollo de aplicaciones","configuración de servidores",1,"Sr");
+        System.out.println(designer.toString());
+        
+        
+        trabajoTI telecom = new trabajoTI("Maria","Desarrollo de aplicaciones","programacion web",3,"leader");
+        System.out.println(telecom.toString());
+        
+        System.out.println("----------------------VIDEOCOMUNICACIÓN--------------------");
+        
     
 }
